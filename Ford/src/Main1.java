c0a7d1e16e9efea263bbd9607b4358a9baf1514d
antochino <antochino@192.168.1.122>
2017-04-07 23:29:04 +0300

    add1

--- Ford/src/Main1.java
+++ Ford/src/Main1.java
@@ -4,6 +4,7 @@ public class Main1 {
 	public static void main(String[] args) {
 		 System.out.println("Hellow!!!");
 		 	System.out.println("hellowvfbbgfb");
+		 	System.out.println("hellowvfbbgfb111111");
 	}
 	
 
